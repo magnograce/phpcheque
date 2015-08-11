0a64156af3efb7a5e77df082b3d7ffbec6676358
Magno dos Santos Braz <below.dark@hotmail.com>
2015-08-11 15:30:10 -0300

    no message

--- index.php
+++ index.php
@@ -11,10 +11,16 @@
     <body>
         <div>
                 <div id="header">
-                    <?php include("menu.php") ?>
+                   <ul class="nav nav-pills">
+                        <li><a href="index.php?pag=home">Home</a></li>
+                        <li><a href="index.php?pag=recebimento">Recebimento</a></li>
+                        <li><a href="pagamento.php?pag=pagamento">Pagamento</a></li>
+                        <li><a href="pessoas.php?pag=pessoas">Pessoas</a></li>
+                        <li><a href="empresas.php?pag=empresas">Empresas</a></li>
+                    </ul>
                 </div>
                 <div id="content">
-                    <?php include("home.php") ?>
+ 
                     
                     <?php
                         $index = 'home';
---
+++ pagamento.php
@@ -0,0 +1,1 @@
+<p> hoje tah legal</p>
